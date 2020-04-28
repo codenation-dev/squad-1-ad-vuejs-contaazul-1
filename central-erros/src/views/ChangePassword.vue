e40de19da406833ff44f7d950e0c8933fdb45c4e
Lucas Peruchi Pereira <lucas_peruchi123@hotmail.com>
2020-04-28 18:05:34 -0300

    alterado layout para telas de login

--- central-erros/src/views/ChangePassword.vue
+++ central-erros/src/views/ChangePassword.vue
@@ -7,42 +7,49 @@
             <article class="card is-rounded card-login">
               <div class="card-content">
                 <h1 class="title">
-                  <img src="../assets/error-log.png" alt="logo" width="200" />
+                  <img src="../assets/login-logo.png" alt="logo" width="200" />
                 </h1>
-                <p class="control has-icon">
+                <p class="control has-icons-left">
                   <input class="input" type="email" placeholder="Email" />
-                  <i class="fa fa-envelope"></i>
+                    <span class="icon is-small is-left padding-icon">
+                      <i class="fa fa-envelope"></i>
+                    </span>
                 </p>
                 <br />
-                <p class="control has-icon">
+                <p class="control has-icons-left">
                   <input
                     class="input"
                     type="password"
                     placeholder="New Password"
                   />
-                  <i class="fa fa-lock"></i>
+                  <span class="icon is-small is-left padding-icon">
+                    <i class="fa fa-lock"></i>
+                  </span>
                 </p>
                 <br />
-                <p class="control has-icon">
+                <p class="control has-icons-left">
                   <input
                     class="input"
                     type="password"
                     placeholder="Confirm new password"
                   />
-                  <i class="fa fa-lock"></i>
+                  <span class="icon is-small is-left padding-icon">
+                    <i class="fa fa-lock"></i>
+                  </span>
                 </p>
+                <br />
                 <br />
                 <p class="columns control">
                   <button
                     @click="redirect('login')"
-                    class="column button button-margin button-padding is-warning is-medium"
+                    class="column button button-margin button-padding password-change is-medium"
                   >
                     <i class="fa fa-exchange-alt icon-space"></i>
                     Change password
                   </button>
                   <button
                     @click="redirect('login')"
-                    class="button-back column button button-padding is-danger is-medium"
+                    class="button-back column button button-padding is-medium"
                   >
                     <i class="fa fa-arrow-left icon-space"></i>
                     Back
@@ -72,13 +79,15 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+
 .card-login {
   width: 30%;
 }
 
 .login-color {
-  background-color: #7ed957;
+  background: rgb(21,72,84);
+  background: linear-gradient(180deg, rgba(21,72,84,1) 0%, rgba(36,83,95,1) 23%, rgba(60,101,113,1) 47%, rgba(99,132,142,1) 71%, rgba(255,255,255,1) 100%);
 }
 
 .login-space {
@@ -97,7 +106,18 @@ export default {
   margin-right: 10px;
 }
 
+.password-change {
+  background-color: #154854;
+  color: #ffffff;
+}
+
 .button-back {
   padding-left: 3px;
+  background-color: #ff7272;
+  color: #ffffff;
+}
+
+.padding-icon {
+  padding-top: 2px;
 }
 </style>
--- central-erros/src/views/Register.vue
+++ central-erros/src/views/Register.vue
@@ -7,31 +7,37 @@
             <article class="card is-rounded card-login">
               <div class="card-content">
                 <h1 class="title">
-                  <img src="../assets/error-log.png" alt="logo" width="200" />
+                  <img src="../assets/login-logo.png" alt="logo" width="200" />
                 </h1>
-                <p class="control has-icon">
+                <p class="control has-icons-left">
                   <input class="input" type="email" placeholder="Email" />
-                  <i class="fa fa-envelope"></i>
+                    <span class="icon is-small is-left padding-icon">
+                      <i class="fa fa-envelope"></i>
+                    </span>
                 </p>
                 <br />
-                <p class="control has-icon">
+                <p class="control has-icons-left">
                   <input class="input" type="password" placeholder="Password" />
-                  <i class="fa fa-lock"></i>
+                    <span class="icon is-small is-left padding-icon">
+                      <i class="fa fa-lock"></i>
+                    </span>
                 </p>
                 <br />
-                <p class="control has-icon">
+                <p class="control has-icons-left">
                   <input
                     class="input"
                     type="password"
                     placeholder="Confirm password"
                   />
-                  <i class="fa fa-lock"></i>
+                    <span class="icon is-small is-left padding-icon">
+                      <i class="fa fa-lock"></i>
+                    </span>
                 </p>
                 <br />
                 <p class="control">
                   <button
                     @click="redirect('login')"
-                    class="button is-info is-medium is-fullwidth"
+                    class="button button-color is-medium is-fullwidth"
                   >
                     <i class="fa fa-user-plus icon-space"></i>
                     Register
@@ -61,13 +67,15 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+
 .card-login {
   width: 30%;
 }
 
 .login-color {
-  background-color: #7ed957;
+  background: rgb(21,72,84);
+  background: linear-gradient(180deg, rgba(21,72,84,1) 0%, rgba(36,83,95,1) 23%, rgba(60,101,113,1) 47%, rgba(99,132,142,1) 71%, rgba(255,255,255,1) 100%);
 }
 
 .login-space {
@@ -76,5 +84,10 @@ export default {
 
 .icon-space {
   margin-right: 5px;
+}
+
+.button-color {
+  background-color: #154854;
+  color: #ffffff;
 }
 </style>
