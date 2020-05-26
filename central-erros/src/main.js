d127facca42d7254560b95aae2a6c50dea4e9165
ElenSouza29 <elencarolinerj@gmail.com>
2020-05-26 16:42:24 -0300

    Vuelidate

--- central-erros/src/main.js
+++ central-erros/src/main.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "@/router/routes";
 import store from "@/store/store";
+import Vuelidate from 'vuelidate';
 
+Vue.use(Vuelidate)
 Vue.config.productionTip = false;
 
 new Vue({
